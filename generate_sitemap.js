a6257d3190081c1262f771e014aca81d414e9462
rakurs-news <sarmatiandon@gmail.com>
2026-07-14 11:15:50 +0300

    Update generate_sitemap.js

--- generate_sitemap.js
+++ generate_sitemap.js
@@ -1,1 +1,25 @@
 
+```javascript
+const fs = require('fs');
+
+const newsData = JSON.parse(fs.readFileSync('news.json', 'utf8'));
+
+let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://rakurs-news.github.io/</loc>
+    <changefreq>daily</changefreq>    <priority>1.0</priority>
+  </url>
+`;
+
+newsData.forEach(news => {    sitemap += `  <url>
+    <loc>https://rakurs-news.github.io/news.html?id=${news.id}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+`;
+});
+
+sitemap += '</urlset>';fs.writeFileSync('sitemap.xml', sitemap);
+console.log(`Готово! Обработано ${newsData.length} новостей.`);
+```
