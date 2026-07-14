df1feec85e78dab2c08b222afb4458c40693f68f
rakurs-news <sarmatiandon@gmail.com>
2026-07-14 11:26:09 +0300

    Update generate_sitemap.js

--- generate_sitemap.js
+++ generate_sitemap.js
@@ -1,25 +1,57 @@
 
-```javascript
 const fs = require('fs');
 
-const newsData = JSON.parse(fs.readFileSync('news.json', 'utf8'));
+const DOMAIN = 'https://rakurs-news.github.io'; // Выносим домен в переменную
 
-let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+function escapeXml(unsafe) {
+    return unsafe.replace(/[<>&'"]/g, function (c) {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case "'": return '&apos;';
+            case '"': return '&quot;';
+        }
+    });
+}
+
+try {
+    // Проверяем наличие файла
+    if (!fs.existsSync('news.json')) {
+        throw new Error('Файл news.json не найден!');
+    }
+
+    const fileContent = fs.readFileSync('news.json', 'utf8');
+    const newsData = JSON.parse(fileContent);
+
+    let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://rakurs-news.github.io/</loc>
-    <changefreq>daily</changefreq>    <priority>1.0</priority>
+    <loc>${DOMAIN}/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
   </url>
 `;
 
-newsData.forEach(news => {    sitemap += `  <url>
-    <loc>https://rakurs-news.github.io/news.html?id=${news.id}</loc>
+    newsData.forEach(news => {
+        // ВАЖНО: экранируем ID, чтобы спецсимволы не сломали XML
+        const safeId = escapeXml(String(news.id));
+        
+        sitemap += `  <url>
+    <loc>${DOMAIN}/news.html?id=${safeId}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
 `;
-});
+    });
 
-sitemap += '</urlset>';fs.writeFileSync('sitemap.xml', sitemap);
-console.log(`Готово! Обработано ${newsData.length} новостей.`);
-```
+    sitemap += '</urlset>';
+
+    // Явно указываем кодировку при записи
+    fs.writeFileSync('sitemap.xml', sitemap, 'utf8');
+    
+    console.log(`Готово! Обработано ${newsData.length} новостей. Файл сохранён как sitemap.xml`);
+} catch (error) {
+    console.error('Ошибка при генерации sitemap:', error.message);
+    process.exit(1);
+}
